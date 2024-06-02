18d9d86b996a0ba4365f6bd54cc6875b4d27ad7f
seif <mohamedahmedseifofficial@gmail.com>
2024-06-02 10:51:30 +0300

    Country list from home page implemented

--- js/countries.js
+++ js/countries.js
@@ -1,7 +1,7 @@
 
-/* Country Description */
+/* Fetch Country Description Json File*/
 const getCountriesDescription = async () => {
-    let response = await fetch('../Local/countryDescription.json');
+    let response = await fetch('../Local/Country-Info.json');
   
     // Check for successful response before parsing JSON
     if (!response.ok) {
@@ -9,38 +9,29 @@ const getCountriesDescription = async () => {
     }
   
     const data = await response.json();
-    const countries = data.Descriptions;
-    return countries;
-  };
-  
-// let dd = getCountriesDescription()
-// console.log(dd.then((data)=>{console.log(data)}));
-
-// let response = fetch('../Local/countryDescription.json').then((response) => {
-//     return response.json();
-// }).then((data) => {
-//     console.log(data);
-//     return data;
-// }).catch((error) => {
-//     console.error('Error:', error);
-// });
+    return data;
+};
 
 let countryPage = document.querySelector(".country-page")
 
 
+/* Load each price category for provided a country name */
 function loadEachPrice(result, priceName) {
     const prices = result[priceName];
     if (!prices) return '';  // Return an empty string if the prices array is not found.
 
-    const items = prices.map((price) => {return `<li>${price.Cost}:</li> <li>${price.Value}<span class=currency>${result.Currency}</span></li>`}).join('');
+    const items = prices.map((price) => {return `<ul class="price-section-wrapper"><li class="price-text">${price.Cost}:</li> <li class="price-currency">${price.Value}<span class=currency>${result.Currency}</span></li></ul>`}).join('');
     return `
         <h2>${priceName}</h2>
-        <ul>${items}</ul>
+        <div class="prices-wrapper">${items}</div>
     `;
 }
+
+/* Load description for each country provided a country name */
 function loadCountryDescription(countryDescriptions, passedCountry) {
     // Use filter to find the matching country
     const matchingCountry = countryDescriptions.filter(country => country.name === passedCountry);
+
   
     // Check if a match is found
     if (matchingCountry.length > 0) {
@@ -48,25 +39,23 @@ function loadCountryDescription(countryDescriptions, passedCountry) {
     } else {
       return "Country description not found."; // Handle no match scenario
     }
-  }
+}
+
+/* Load country information when document loads */
 document.addEventListener('DOMContentLoaded', async (event) => {
+
+    console.log('Document has been fully loaded and parsed');
 
     let countryDescription = await getCountriesDescription();
     console.log(countryDescription);
 
-   console.log(loadCountryDescription(countryDescription, "Egypt"));
-
-
-    console.log('Document has been fully loaded and parsed');
     let countriesString = localStorage.getItem("results")
-    console.log(countriesString)
-    const resultsObject = await JSON.parse(countriesString);
-    console.log(resultsObject)
 
-    // countryPage.innerHTML = `Country : ${resultsObject['Country Name']}`
+    const resultsObject = await JSON.parse(countriesString);
+
     countryPage.innerHTML = `
-    <div class="country-page-container">
-        <h1 class="country-page-header">${resultsObject['Country Name']} is a great country with beautiful touristic destinations discover everything about prices, transportations and more</h1>
+    <div class="country-page-container container w-75 fs-1">
+        <h1 class="country-page-header my-5">${resultsObject['Country Name']} is a great country with beautiful touristic destinations discover everything about prices, transportations and more</h1>
         <div class="country-description">${loadCountryDescription(countryDescription,resultsObject['Country Name'])}</div>
         <div class="apartment-prices">${loadEachPrice(resultsObject, "Buy Apartment prices")}</div>
         <div class="childcare-prices">${loadEachPrice(resultsObject, "Childcare prices")}</div>
